343507646d680713ca9999df5bc83e52e83b6d71
samimhakimi <samimgooogl@gmail.com>
2022-04-03 15:45:05 -0400

    Initial Commit

---
+++ Traveler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traveler
+//
+//  Created by Samim Hakimi on 4/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
